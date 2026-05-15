aee91c892472ecb5d65bbdd4d0a528f942cd6a4d
kkohlmeyer-beep <k.kohlmeyer@web.de>
2026-05-15 12:46:44 +0200

    Layout

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 const data = [
-  ["Pflegedienst", "Zuhause bei Katrin", "Zuhause bei Katrin", "Zuhause", "Zuhause bei Katrin", "Katrin"], // Sonntag
-  ["Pflegedienst", "Tagespflege", "Tagespflege", "Zuhause", "Zuhause bei Katrin", "Pflegedienst"], // Montag
-  ["Pflegedienst", "Tagespflege", "Tagespflege", "Zuhause", "Zuhause bei Katrin", "Katrin"], // Dienstag
-  ["Pflegedienst", "Tagespflege", "Tagespflege", "Zuhause", "Zuhause bei Katrin", "Katrin"], // Mittwoch
-  ["Pflegedienst", "Tagespflege", "Tagespflege", "Zuhause", "Zuhause bei Katrin", "Katrin"], // Donnerstag
-  ["Pflegedienst", "Tagespflege", "Tagespflege", "Zuhause", "Zuhause bei Katrin", "Katrin"], // Freitag
-  ["Pflegedienst", "Zuhause bei Katrin", "Zuhause bei Katrin", "Zuhause", "Zuhause bei Katrin", "Katrin"] // Samstag
+    //Morgens:         Frühstück:		           Mittagessen:		        Nachmittags:	  Abendessen:		             Abends:
+  ["Pflegedienst",	   "Zuhause bei Katrin",	 "Zuhause bei Katrin",  "Zuhause",	    "Zuhause bei Katrin",	     "Katrin"],		      // Sonntag
+  ["Pflegedienst",	   "Tagespflege",          "Tagespflege",         "Zuhause",    	"Zuhause bei Katrin", 	   "Pflegedienst"],   // Montag
+  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",    	"Zuhause bei Katrin", 	   "Katrin"],         // Dienstag
+  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",    	"Zuhause bei Katrin",      "Katrin"],         // Mittwoch
+  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",    	"Zuhause bei Katrin", 	   "Katrin"],         // Donnerstag
+  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",    	"Zuhause bei Katrin",      "Katrin"],         // Freitag
+  ["Pflegedienst", 	   "Zuhause bei Katrin", 	"Zuhause bei Katrin", 	"Zuhause",    	"Zuhause bei Katrin",      "Katrin"]          // Samstag
 ];
-

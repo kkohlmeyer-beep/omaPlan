9705c89dde2b8874422201d30bff5a30109455d3
kkohlmeyer-beep <k.kohlmeyer@web.de>
2026-05-15 12:47:32 +0200

    Create data-vorlage.js

---
+++ data-vorlage.js
@@ -0,0 +1,10 @@
+const data = [
+    //Morgens:         Frühstück:		           Mittagessen:		        Nachmittags:	  Abendessen:		             Abends:
+  ["Pflegedienst",	   "Zuhause bei Katrin",	 "Zuhause bei Katrin",  "Zuhause",	    "Zuhause bei Katrin",	     "Katrin"],		      // Sonntag
+  ["Pflegedienst",	   "Tagespflege",          "Tagespflege",         "Zuhause",    	"Zuhause bei Katrin", 	   "Pflegedienst"],   // Montag
+  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",    	"Zuhause bei Katrin", 	   "Katrin"],         // Dienstag
+  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",    	"Zuhause bei Katrin",      "Katrin"],         // Mittwoch
+  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",    	"Zuhause bei Katrin", 	   "Katrin"],         // Donnerstag
+  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",    	"Zuhause bei Katrin",      "Katrin"],         // Freitag
+  ["Pflegedienst", 	   "Zuhause bei Katrin", 	"Zuhause bei Katrin", 	"Zuhause",    	"Zuhause bei Katrin",      "Katrin"]          // Samstag
+];

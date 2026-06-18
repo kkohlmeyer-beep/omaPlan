95ee9f3b616032aaf6561da0d6bb52830983d786
kkohlmeyer-beep <k.kohlmeyer@web.de>
2026-06-18 07:01:41 +0200

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const data = [
   ["Pflegedienst",	   "Tagespflege",            "Tagespflege",         "Zuhause",            "Zuhause", 	   "Pflegedienst"],   // Montag
   ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",            "Zuhause", 	   "Pflegedienst"],         // Dienstag
   ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",            "bei Katrin",        "Katrin"],         // Mittwoch
-  ["Pflegedienst",	   "Zuhause, dann Zahnarzt",	         "Tagespflege",          "Zuhause",	          "bei Katrin",	       "Katrin"],         // Donnerstag
+  ["Pflegedienst",	   "Zuhause, dann Augenarzt",	         "Tagespflege",          "Zuhause",	          "bei Katrin",	       "Katrin"],         // Donnerstag
   ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",            "bei Katrin",           "Katrin"],         // Freitag
   ["Pflegedienst", 	   "bei Katrin",                "bei Katrin",               "Zuhause",    	          "bei Katrin",            "Katrin"]          // Samstag
 ];

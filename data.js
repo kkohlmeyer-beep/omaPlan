b21b5984819a7574c5394e010ca3789574f7043b
kkohlmeyer-beep <k.kohlmeyer@web.de>
2026-06-28 08:43:19 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 const data = [
     //Morgens:         Frühstück:		           Mittagessen:		        Nachmittags:	  Abendessen:		             Abends:
-  ["Pflegedienst",	   "bei Katrin",	             "Zuhause",             "Einladung bei Lüpke",	          "Zuhause",	       "Katrin"],	      // Sonntag
-  ["Pflegedienst",	   "Tagespflege",            "Tagespflege",         "Zuhause",            "Zuhause", 	   "Pflegedienst"],   // Montag
-  ["Pflegedienst", 	   "Zahnarzt / Tagespflege", 		     "Tagespflege",         "Zuhause",            "Zuhause", 	   "Katrin"],         // Dienstag
-  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",            "Zuhause",        "Katrin"],         // Mittwoch
-  ["Pflegedienst",	   "Tagespflege",	         "Tagespflege",          "Zuhause",	          "Zuhause",	       "Katrin"],         // Donnerstag
-  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",         "Zuhause",            "Zuhause",           "Katrin"],         // Freitag
-  ["Pflegedienst", 	   "bei Katrin",                "bei Katrin",               "Zuhause",    	          "bei Katrin",            "Katrin"]          // Samstag
+  ["Pflegedienst",	   "bei Katrin",	             "Zuhause",             "Zuhause",	          "bei Katrin",	       "Katrin"],	      // Sonntag
+  ["Pflegedienst",	   "Tagespflege",            "Tagespflege",             "Zuhause",            "Zuhause", 	       "Pflegedienst"],   // Montag
+  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",             "Zuhause",            "Zuhause", 	       "Katrin"],         // Dienstag 
+  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",             "Zuhause",            "Zuhause",           "Katrin"],         // Mittwoch
+  ["Pflegedienst",	   "Tagespflege",	         "Tagespflege",             "Zuhause",	          "Zuhause",	       "Katrin"],         // Donnerstag
+  ["Pflegedienst", 	   "Tagespflege", 		     "Tagespflege",             "Zuhause",            "Zuhause",           "Katrin"],         // Freitag
+  ["Pflegedienst", 	   "bei Katrin",                "bei Katrin",           "Zuhause",    	      "Zuhause",           "Katrin"]          // Samstag
 ];
